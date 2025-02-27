c9a2cde5b6a5972f18e0d679c615d0977e74f9a2
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2025-02-27 12:07:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,607 @@
-const crudList = document.querySelector('.crud-list')
+const dataGk = [{
+        ques: "first winter olympics ?",
+        cop: "1921 india",
+        bop: "1920 canada",
+        aop: "1923 singapore",
+        ans: "1924 chamonise france",
+},
+{
+        ques: "AIBA boxing bout boys and girls each round ?",
+        bop: "1 min",
+        ans: "2 min",
+        aop: "3 min",
+        cop: "4 min",
+},
+{
+        ques: "asian games 2018 ?",
+        bop: " india,delhi,",
+        aop: "japan,tokyo",
+        ans: "indonesia,jakrata polenburg",
+        cop: "china,beijing",
+},
+{
+        ques: "first asian games ?",
+        ans: " india,delhi ",
+        cop: "japan,tokyo",
+        aop: "indonesia,jakrata polenburg",
+        bop: "china,beijing",
+},
+{
+        ques: "first asian games ?",
+        cop: " 1954 ",
+        bop: "1965",
+        ans: "1951",
+        aop: "1960",
+},
+{
+        ques: "most asian games ?",
+        aop: " india,delhi",
+        ans: "thailand,bangkok",
+        cop: "indonesia.indos",
+        bop: "japan,tokyo",
+},
+{
+        ques: "when india host asian games ?",
+        ans: " 1951,1982",
+        aop: "1960,1967",
+        bop: "1954,1951",
+        cop: "1987,1982",
+},
+{
+        ques: "when thailand host asian games ?",
+        bop: " 1951,1982,1960,1964",
+        ans: "1966,1970,1978,1998",
+        cop: "1954,1951,1968,1969",
+        aop: "1987,1982,1980,1955",
+},
+{
+        ques: "fist commonwealth youth games ?",
+        ans: "scotland,edinburg,2000",
+        bop: "england,london,1999",
+        cop: "canada,hamilton,2002",
+        aop: "usa,california,1995",
+},
+{
+        ques: "commonwealth games 2022?",
+        aop: "scotland,edinburg",
+        cop: "england,london",
+        bop: "canada,hamilton",
+        ans: "uk,birmingham",
+},
+{
+        ques: " commonwealth games 2018?",
+        bop: "netherland",
+        ans: "gold coast",
+        aop: "ghana",
+        cop: "india",
+},
+{
+        ques: "which country only first host commonwealth games 2010?",
+        ans: "india",
+        cop: "australia",
+        bop: "china",
+        aop: "bangladesh",
+},
+{
+        ques: "avg circumference of international cricket ball?",
+        ans: "8.81 - 9 inch(21 to 25 cm)",
+        aop: "8 - 10 inch(19 to 26 cm)",
+        cop: "6 - 7 inch(15 to 20 cm)",
+        bop: "4.45 - 6 inch(12 to 17 cm)",
+},
+{
+        ques: "avg weight of international cricket ball?",
+        ans: "155.9 to 163 gm",
+        aop: "150 to 164 gm",
+        bop: "149 to 161 gm",
+        cop: "150.8 to 157 gm",
+},
+{
+        ques: "table tennis dimension?",
+        ans: "length- 2.74 m(274cm),width-1.525m(152.5cm),height-.76m(76cm)",
+        aop: "length- 2.70 m(270cm),width-1.545m(154.5cm),height-.70m(70cm)",
+        bop: "length- 2.98 m(298cm),width-1.498m(149.8cm),height-.66m(66cm)",
+        cop: "length- 2.75 m(275cm),width-1.545m(154.5cm),height-.76m(76cm)",
+},
+{
+        ques: "badminton tennis dimension?",
+        aop: "length- 13.9m,width-6.1m,height-6ft or 6ft 1 inch",
+        bop: "length- 14.1m,width-7.25m,height-5ft or 5ft 1 inch",
+        ans: "length- 13.9m,width-6.1m,height-5ft or 5ft 1 inch",
+        cop: "length- 12m,width-5.4m,height-4ft",
+},
+{
+        ques: "goal post acc fifa dimension?",
+        ans: "height-8ft(2.44m),width-7.32m(8yard)",
+        cop: "height-9.5ft(2.67m),width-8.5m(10yard)",
+        bop: "height-7ft(2.32m),width-9m(12yard)",
+        aop: "height-7.72ft(2.75m),width-7m(7yard)",
+},
+{
+        ques: "dist b/w stump and popping crease?",
+        cop: "1.32m",
+        bop: "1.43m",
+        ans: "1.22m",
+        aop: "1.5m",
+},
+{
+        ques: "free throw basketball point?",
+        cop: "2",
+        ans: "1",
+        bop: "3",
+        aop: "5",
+},
+{
+        ques: "max point in free throw basketball ?",
+        cop: "3",
+        aop: "5",
+        bop: "6",
+        ans: "4",
+},
+{
+        ques: "dist b/w penalty mark to mid point football?",
+        bop: "12m",
+        cop: "14.1m",
+        aop: "10.11",
+        ans: "11m",
+},
+{
+        ques: "tennis compelete score set?",
+        aop: "4-6",
+        bop: "5-4",
+        cop: "5-5",
+        ans: "6-4",
+},
+{
+        ques: "tennis tie ?",
+        cop: "reuce",
+        bop: "ping pong",
+        ans: "deuce",
+        aop: "tiedi",
+},
+{
+        ques: "tennis advantage-out ?",
+        cop: "starter 2 point",
+        aop: "server 2 point",
+        ans: "receiver 2 point",
+        bop: "equal point",
+},
+{
+        ques: "tennis advantage-in?",
+        aop: "starter 2 point",
+        ans: "server 2 point",
+        cop: "receiver 2 point",
+        bop: "equal point",
+},
+{
+        ques: "FIVB first men volleyball world cup",
+        op: "1967,west coast",
+        ans: "1969,east germany",
+        op: "1970,hamilton canada",
+        op: "1980,india bhuneshwar",
+},
+{
+        ques: "basketball center line name",
+        aop: "back court part",
+        cop: "dunk court part",
+        ans: "mid point part",
+        bop: "dribbling court part",
+},
+{
+        ques: "basketball dimension",
+        cop: "length-95m,width-56m",
+        ans: "length-94m,width-50m",
+        bop: "length-91m,width-50m",
+        aop: "length-97m,width-54m",
+},
+{
+        ques: "poona sports",
+        aop: "hockey",
+        ans: "badminton",
+        cop: "tennis",
+        bop: "cricket",
+},
+{
+        ques: "marathon race total distance?",
+        bop: "41km",
+        ans: "42.195km",
+        aop: "43km",
+        cop: "44.122km",
 
-const list = document.querySelector('.list')
+},
+{
+        ques: "in how many ways batsmen can be out?",
+        aop: "9",
+        bop: "11",
+        cop: "5",
+        ans: "10",
 
-let items = []
-function handleSubmit(e){
-    e.preventDefault()
-const name = e.currentTarget.item.value
-if(!name)return
-const item = {
-    name:name,
-id : Date.now(),
-complete:false,
-}
-items.push(item)
-e.target.reset()
+},
+{
+        ques: "which country 2032 summer olympics?",
+        cop: "india",
+        bop: "bangladesh",
+        aop: "usa",
+        ans: "australia",
 
-list.dispatchEvent(new CustomEvent("ItemsUpdated"))
-}
+},
+{
+        ques: "which country held women cricket 9th world cup 2024?",
+        bop: "pakistan",
+        cop: "india",
+        aop: "sri lanka",
+        ans: "bangladesh",
 
-function displayList() {
-const html = items.map(item=>
-    ` <li class="shopping-list">
-        <input type="checkbox" name="checkbox" value="${item.id}" class="checkbox" ${item.complete?"checked":""}>
-        <span class="itemName">${item.name}</span>
-    <button value="${item.id}" aria-label="Remove ${item.name}" class="remove">X</button></li>`
-).join("")
-list.innerHTML = html;
-}
+},
+{
+        ques: "trap shooting other name?",
+        aop: "hunter",
+        bop: "eye hunt",
+        ans: "clay pigeon",
+        cop: "shooting",
 
-function mirrotToLocalStorage(){
-    localStorage.setItem("items",JSON.stringify(items))
+},
+{
+        ques: "LONA term related to which game ?",
+        bop: "cricket, full team out",
+        aop: "basketball, perfect score",
+        ans: "kabaddi,full team out",
+        cop: "cricket, half team out",
 
-}
-function getDataFromLocalStorga(){
-    const localStorageItems = JSON.parse(localStorage.getItem("items"))
-    if(localStorageItems.length){
-        items = localStorageItems
-        // items.push(...localStorageItems)
-list.dispatchEvent(new CustomEvent("ItemsUpdated"))
-    }
-}
+},
+{
+        ques: "when pro kabaddi start who win this?",
+        cop: "2016, haryana panther",
+        bop: "2015, punjab bull",
+        ans: "2014, jaipur pink panther",
+        aop: "2012,tamil lion",
+
+},
+
+{
+        ques: "first to host two times paralympics?",
+        cop: "beijing",
+        ans: "tokyo",
+        aop: "paris",
+        bop: "california",
+
+},
+{
+        ques: "how big is circle yard in cricket and how many player can play outside of this?",
+        bop: "32 yards, 3",
+        ans: "30 yards,2",
+        cop: "32 yards,2",
+        aop: "21 yards, 5",
+
+},
+
+{
+        ques: "europeon championship or heineken cup related to which games?",
+        cop: "cricket",
+        bop: "basketball",
+        aop: "football",
+        ans: "rugby"
+},
+{
+        ques: "when indian football association established ?",
+        bop: "chennai, 1899",
+        aop: "bombay,1896",
+        ans: "kolkata,1893",
+        cop: "punjab,1895",
+
+},
+{
+        ques: "which country has won most title of asian football championship?",
+        cop: "china",
+        aop: "singapore",
+        bop: "thailand",
+        ans: "japan",
+
+},
+{
+        ques: "Tennis court double width ?",
+        bop: "11m",
+        cop: "10.91m",
+        aop: "11.95m",
+        ans: "10.97m",
+
+},
+{
+        ques: "when sakshi malik won bronze medal and in which weight category?",
+        ans: "2016,58kg",
+        bop: "2018,68kg",
+        aop: "2019,65kg",
+        cop: "2017,60kg",
+
+},
+{
+        ques: "Davis cup related to which game ?",
+        cop: "cricket",
+        bop: "basketball",
+        aop: "kabaddi",
+        ans: "tennis",
+
+},
+{
+        ques: "where 2023 mens hockey world cup held ?",
+        aop: "srinagar",
+        ans: "orrisa",
+        cop: "maharashtra",
+        bop: "gujarat",
+
+},
+{
+        ques: "1st ODI of cricket ?",
+        aop: "1972",
+        ans: "1971",
+        cop: "1968",
+        bop: "1956",
+
+},
+{
+        ques: "1st men world cup ?",
+        bop: "1972",
+        aop: "1973",
+        cop: "1968",
+        ans: "1975",
+},
+{
+        ques: "1st women world cup ?",
+        cop: "1972",
+        ans: "1973",
+        bop: "1968",
+        aop: "1956",
+},
+{
+        ques: "differnce of year in winter or summer olympics ?",
+        bop: "3",
+        cop: "4",
+        ans: "2",
+        aop: "5",
+},
+{
+        ques: "ICC world cup held by india how many times and when ?",
+        ans: " 1987, 1996, 2011, 2023",
+        aop: "1983, 1990, 2011, 2023",
+        bop: "1968, 1993, 2007, 2015",
+        cop: "1956, 2011, 2019, 2023",
+},
+{
+        ques: "beach volleyball dimension ?",
+        ans: "length 16m, width 8m",
+        aop: "length 15m, width 9m",
+        bop: "length 17m, width 8m",
+        cop: "length 20m, width 10m",
+},
+{
+        ques: "after stop dribbling in how many seconds have to pass ball ?",
+        aop: "4",
+        bop: "7",
+        cop: "10",
+        ans: "3",
+},
+{
+        ques: "indian olympic association established ?",
+        bop: "1925",
+        ans: "1927",
+        aop: "1920",
+        cop: "1930",
+},
+{
+        ques: "when india host t20 world cup ?",
+        aop: "2014",
+        ans: "2016",
+        cop: "2010",
+        bop: "2018",
+},
+{
+        ques: "when india organised asian wrestling championship and in which city ?",
+        ans: "jalandhar, punjab, 1979",
+        aop: "new delhi, 1981",
+        cop: "orrisa, bhuneshwar, 1980",
+        bop: "tamil nadu, chennai, 1973",
+},
+{
+        ques: "FIBA game of basketball have how many quarters and of how much minute ?",
+        ans: "4, 10",
+        aop: "7, 6",
+        bop: "6, 5",
+        cop: "2, 15",
+},
+{
+        ques: "where 2018 FIH hockey world cup held and who won ?",
+        bop: "jalandhar, punjab, pakistan",
+        cop: "new delhi, india",
+        ans: "orrisa, bhuneshwar, belgium",
+        aop: "tamil nadu, chennai, germany",
+},
+{
+        ques: "where 2023 FIH hockey world cup held and who won ?",
+        aop: "jalandhar, punjab, pakistan",
+        cop: "new delhi, india",
+        ans: "orrisa, bhuneshwar, germany",
+        bop: "tamil nadu, chennai, belgium",
+},
+{
+        ques: "which country won FIH hockey world cup most times ?",
+        ans: " pakistan",
+        aop: " india",
+        cop: " belgium",
+        bop: " germany",
+},
+{
+        ques: "duration of hockey games ?",
+        ans: "60 min",
+        aop: "40 min",
+        cop: "50 min",
+        bop: "90 min",
+},
+{
+        ques: "how many quarters have in  hockey games ?",
+        cop: "6, 10 min",
+        ans: "4, 15 min",
+        bop: "5, 10 min",
+        aop: "6, 15 min",
+},
+{
+        ques: "duration of football games ?",
+        cop: "60 min",
+        aop: "40 min",
+        bop: "50 min",
+        ans: "90 min",
+},
+{
+        ques: "break of football games ?",
+        cop: "10 min",
+        ans: "15 min",
+        bop: "25 min",
+        aop: "30 min",
+},
+{
+        ques: "break of test cricket games ?",
+        cop: "60 min",
+        ans: "40 min",
+        bop: "50 min",
+        aop: "90 min",
+},
+{
+        ques: "how many overs in test cricket by one team ?",
+        bop: "60 ",
+        cop: "40 ",
+        aop: "50",
+        ans: "90",
+},
+{
+        ques: "FIFA headquarter?",
+        aop: "france",
+        cop: "brazil",
+        bop: "argentina",
+        ans: "zurich",
+},
+{
+        ques: "first fifa world cup ?",
+        ans: "1930",
+        cop: "1925",
+        aop: "1920",
+        bop: "1927",
+},
+{
+        ques: "which country won first fifa and where it held ?",
+        cop: "brazil, argentina",
+        bop: "portuguese, urugway",
+        ans: "urugway, urugway",
+        aop: "france, england",
+},
+{
+        ques: "first t20 world cup held in which country and who won?",
+        ans: "2007, south africa, india",
+        cop: "2009, england, australia",
+        bop: "2010, west indies, new zealand",
+        aop: "2008, sri lanka, australia",
+},
+{
+        ques: "first women t20 world cup held and who won?",
+        cop: "2007, south africa",
+        ans: "2009, england",
+        bop: "2010, west indies",
+        aop: "2008, sri lanka",
+},
+{
+        ques: "8th women t20 world cup held in which country and who won?",
+        ans: " south africa, australia",
+        cop: " england, india",
+        bop: " west indies, new zealand",
+        aop: " sri lanka, australia",
+},
+{
+        ques: "indian football association established?",
+        cop: "1894, chennai",
+        bop: "1895, jalandhar",
+        ans: "1893, kolkata",
+        aop: "1890,  bombay",
+},
+{
+        ques: "ICC established and where its headquarter?",
+        cop: "1910, england",
+        ans: "1909, dubai",
+        bop: "1919, australia",
+        aop: "1930, sri lanka",
+},
+{
+        ques: "oldest tennis tournament?",
+        ans: "wimbledon, 1877",
+        cop: "1860, french",
+        bop: "1869, aurtralian",
+        aop: "1875, usa",
+},
+{
+        ques: "when first youth olympics games start?",
+        cop: "2009",
+        ans: "2010",
+        bop: "2007",
+        aop: "2012",
+},
+{
+        ques: "where first youth olympics games start?",
+        cop: "england",
+        bop: "china",
+        ans: "singapore",
+        aop: "france",
+},
 
 
-function deleteItems(id){
-    items = items.filter(item=>item.id!==id)
-    list.dispatchEvent(new CustomEvent("ItemsUpdated"))
-
-}
-function markAsComplete(id){
-    const itemRef = items.find(item=> item.id === id)
-    itemRef.complete = !itemRef.complete
-    list.dispatchEvent(new CustomEvent("ItemsUpdated"))
-}
-
-crudList.addEventListener("submit", handleSubmit)
-list.addEventListener("ItemsUpdated",displayList)
-list.addEventListener("ItemsUpdated",mirrotToLocalStorage)
 
 
-list.addEventListener("click", function(e){
-    const id = parseInt(e.target.value)
-    if(e.target.matches("button")){
-        deleteItems(id)
-    }
-    if(e.target.matches('input[type="checkbox"]')){
-        markAsComplete(id)
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
+
+
+const gameHtml = dataGk.map(e =>
+        `<div class="quesCol">
+                        <div class="ques">${e.ques}</div>
+                         <div class="op">${e.aop}</div>
+                          <div class="op">${e.cop}</div>
+                           <div class="ans">${e.ans}</div>
+                            <div class="op">${e.bop}</div>
+                        </div>
+                        
+                        
+                        `
+).join('')
+const game = document.querySelector(".game")
+game.innerHTML = gameHtml
+
+let ans = document.querySelectorAll(".ans")
+ans.forEach(e => {
+        e.addEventListener("click", (i) => {
+                i.target.classList.add("green")
+        })
+
 })
-getDataFromLocalStorga()
