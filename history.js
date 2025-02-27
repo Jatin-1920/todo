f58d2528c709ee226a6fcda452e11326ceae75f3
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2025-02-27 12:05:01 +0530

    Update history.js

--- history.js
+++ history.js
@@ -1,1 +1,162 @@
-h
+const histData =[
+    {
+        ques: "who found brahmin kingdom?",
+        aop: "vasudev",
+        bop: "susharma",
+        cop: "shimuk",
+        ans: "pushyamitra shungh",
+
+    },
+    {
+        ques: "indo yunani king meanders defeated by?",
+        aop: "vasudev",
+        bop: "susharma",
+        cop: "shimuk",
+        ans: "pushyamitra shungh",
+
+    },
+    {
+        ques: "who is two times ashvamegh yagh by patanjali?",
+        aop: "vasudev",
+        bop: "susharma",
+        cop: "shimuk",
+        ans: "pushyamitra shungh",
+
+    },
+    {
+        ques: "who made barhut stupa?",
+        aop: "vasudev",
+        bop: "susharma",
+        cop: "shimuk",
+        ans: "pushyamitra shungh",
+
+    },
+    {
+        ques: "who killed last king devbhuti?",
+        aop: "pushyamitra sungh",
+        bop: "susharma",
+        cop: "shimuk",
+        ans: "vasudev",
+
+    },
+    {
+        ques: "who found kanava vansh?",
+        ans: "vasudev",
+        bop: "susharma",
+        cop: "shimuk",
+        aop: "pushyamitra shungh",
+
+    },
+    {
+        ques: "who is last king of kanava vansh?",
+        aop: "vasudev",
+        ans: "susharma",
+        cop: "shimuk",
+        bop: "pushyamitra shungh",
+
+    },
+    {
+        ques: "who killed susharma?",
+        aop: "vasudev",
+        bop: "susharma",
+        cop: "chandragupta",
+        ans: "shimuk",
+
+    },
+    {
+        ques: "who founded stavahna kingdom?",
+        aop: "vasudev",
+        bop: "susharma",
+        cop: "chandragupta",
+        ans: "shimuk",
+
+    },
+    {
+        ques: "which kingdom capital is vidisha?",
+        aop: "vasudev",
+        bop: "susharma",
+        cop: "chandragupta",
+        ans: "pushyamitra sungha",
+
+    },
+    {
+        ques: "pratisthan is capital of which kingdom?",
+        aop: "vasudev",
+        bop: "susharma",
+        cop: "chandragupta",
+        ans: "satvahna",
+
+    },
+    {
+        ques: "ashvamegh or rajshuya is yag of which kingdom?",
+        aop: "vasudev",
+        bop: "susharma",
+        cop: "chandragupta",
+        ans: "satvahna or satkarni",
+
+    },
+    {
+        ques: "gathasaptati is book of which author?",
+        aop: "gunadaya",
+        bop: "susharma",
+        cop: "chandragupta",
+        ans: "haal",
+
+    },
+    {
+        ques: "who use first lead in which rule?",
+        aop: "vasudev",
+        bop: "susharma",
+        cop: "chandragupta",
+        ans: "sungh",
+
+    },
+    {
+        ques: "sungh import lead from which?",
+        aop: "china",
+        bop: "greece",
+        cop: "iran",
+        ans: "rome",
+
+    },
+    {
+        ques: "who start first land donation to brahmin",
+        aop: "vasudev",
+        bop: "susharma",
+        cop: "chandragupta",
+        ans: "satvahna",
+
+    },
+    {
+        ques: "karle ka chaitya or ajanta or ellora cave is work of which kingdom?",
+        aop: "vasudev",
+        bop: "susharma",
+        cop: "chandragupta",
+        ans: "satvahana",
+
+    },
+    {
+        ques: "who known as southern swami?",
+        aop: "vasudev",
+        bop: "susharma",
+        cop: "chandragupta",
+        ans: "satvahana",
+
+    },
+    {
+        ques: "which known as setu as bridge north to south india?",
+        aop: "vasudev",
+        bop: "susharma",
+        cop: "chandragupta",
+        ans: "satvahana",
+
+    },
+    {
+        ques: "karle ka chaitya or ajanta or ellora cave is work of which kingdom?",
+        aop: "vasudev",
+        bop: "susharma",
+        cop: "chandragupta",
+        ans: "satvahana",
+
+    },
+]
